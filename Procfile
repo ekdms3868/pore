a3182d8ce79e3d7223ecaf96af63f30b3e6cce5c
daeun <ehsjt3868@naver.com>
2021-08-12 00:35:21 +0900

    1

---
+++ Procfile
@@ -0,0 +1,1 @@
+web: gunicorn pore.wsgi --log-file -
--- pore/settings.py
+++ pore/settings.py
@@ -21,12 +21,14 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-jh0)9qlz1x@m4&*lwlj(3usf&++xk@p4ipkge#0f7p-3wt6c+4'
+# SECRET_KEY = 'django-insecure-jh0)9qlz1x@m4&*lwlj(3usf&++xk@p4ipkge#0f7p-3wt6c+4'
+SECRET_KEY = os.environ.get('DJANGO_SECRET_KEY','django-insecure-jh0)9qlz1x@m4&*lwlj(3usf&++xk@p4ipkge#0f7p-3wt6c+4')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = bool(os.environ.get('DJANGO_DEBUG', True))
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
+
 
 
 # Application definition
@@ -48,6 +50,7 @@ INSTALLED_APPS = [
 ]
 
 MIDDLEWARE = [
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
@@ -147,3 +150,7 @@ X_FRAME_OPTIONS = 'SAMEORIGIN'
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+import dj_database_url
+db_from_env = dj_database_url.config(conn_max_age=500)
+DATABASES['default'].update(db_from_env)
